3e976f94c8a815fa38ff1d16fa6991deb52fc8cd
Rakhel Cakra Kusumadinata Sera <rakelsera2001@gmail.com>
2023-10-04 19:41:55 +0700

    sidebar done

--- frontend-antybullying/src/main.jsx
+++ frontend-antybullying/src/main.jsx
@@ -5,13 +5,12 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
+import DashboardsTeacher from './pages/teacher'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element:     <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>,
+    element: <DashboardsTeacher />,
   },
 ]);
 
---
+++ frontend-antybullying/src/pages/teacher/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Sidebar from '../../components/teacher/Sidebar'
+
+
+export default function index() {
+  return (
+    <>
+        <Sidebar />
+    </>
+  )
+}
